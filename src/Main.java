2d1f3b07dc3eb9d3f944282d6661fe9a0036fd54
Андрей <najd1703@mailm.ru>
2024-06-09 17:34:13 +0300

    DR-1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,68 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog * 4;
+        System.out.println(dog);
+        cat = cat *4;
+        System.out.println(cat);
+        paper = paper * 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var boxerMass1 = 78.2;
+        System.out.println(boxerMass1);
+        var boxerMass2 = 82.7;
+        System.out.println(boxerMass2);
+        var theAmount = boxerMass1 + boxerMass2;
+        System.out.println("Общий весс 2х боксёров " + theAmount + " кг!");
+        var overLoad = boxerMass2 % boxerMass1;
+        System.out.println("разница в весе на " + overLoad + " кг!");
+
+        var time = 640;
+        var hours = 8;
+        var peaple = time / hours;
+        System.out.println("в компании работает " + peaple + " сотрудников");
+        var peapleCompany = peaple + 94;
+        System.out.println("в компании работает " + peapleCompany + " сотрудников");
+        var timeWork = peapleCompany * hours;
+        System.out.println( + timeWork + " такое кол-во часов должно быть поделенно между сотрудниками");
+        System.out.println("Если в компании работает " + peapleCompany + " сотрудников то, всего " + timeWork + " часов работы может быть поделено между сотрудниками" );
+
+
+
+
+
+
+
+
     }
 
 }
